f19c1a57f0561de4b7832b977d4b368e6475b72c
Fadri Furrer <ffurrer@gmail.com>
2014-04-09 18:02:50 +0200

    mav_model: updated motor_controller and multi_copter
    This two files will most likely never be used, since we went for another
    design, which was implemented in the mav_control branch

--- mav_model/include/mav_model/motor_controller.hpp
+++ mav_model/include/mav_model/motor_controller.hpp
@@ -9,15 +9,14 @@
 #define MAV_MODEL_MOTOR_CONTROLLER_H
 #include <Eigen/Eigen>
 
-template<class InputT, class ParamsT>
+
 class MotorController
 {
   public:
-    MotorController(int amount_motors) : 
+    MotorController(int amount_motors) :
       ref_rotor_rot_vels_(Eigen::VectorXd::Zero(amount_motors)){};
     virtual ~MotorController();
-    Eigen::VectorXd getMotorVelocities(InputT input_reference) {
-      input_reference_ = input_reference;
+    void getMotorVelocities() {
       calculateRefMotorVelocities();
       return ref_rotor_rot_vels_;
     }
@@ -28,11 +27,13 @@ class MotorController
 
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
   protected:
-    InputT input_reference_;
     // imu_
     // odom_
+    
+    Eigen::Vector3d position_;
+    Eigen::Vector3d velocity_;
+    Eigen::Quaternion attitude_;
     Eigen::VectorXd ref_rotor_rot_vels_;
-    ParamsT params;
 };
 
 #endif // MAV_MODEL_MOTOR_CONTROLLER_H
