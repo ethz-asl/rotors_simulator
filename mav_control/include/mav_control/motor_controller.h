cc34dc457a7b8fbfd659628b49da0d14920b79df
Michael Burri <burri210@gmail.com>
2014-07-01 16:22:15 +0200

    added eigen aligned operator

--- mav_control/include/mav_control/motor_controller.h
+++ mav_control/include/mav_control/motor_controller.h
@@ -10,6 +10,7 @@ class MotorController : public ControllerBase {
     virtual std::shared_ptr<ControllerBase> Clone();
     virtual void CalculateRotorVelocities(Eigen::VectorXd* rotor_velocities) const;
 
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
   private:
     Eigen::Matrix4Xd allocation_matrix_;
     Eigen::MatrixX4d angular_acc_to_rotor_velocities_;
