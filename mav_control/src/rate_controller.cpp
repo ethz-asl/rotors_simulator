6a14df16d2c62d5c6c7989135039158920691b31
Michael Burri <burri210@gmail.com>
2014-07-03 10:35:09 +0200

    alignement and eigen fixes

--- mav_control/src/rate_controller.cpp
+++ mav_control/src/rate_controller.cpp
@@ -7,7 +7,7 @@ RateController::RateController() {}
 RateController::~RateController() {}
 
 std::shared_ptr<ControllerBase> RateController::Clone() {
-  std::shared_ptr<ControllerBase> controller = std::make_shared<RateController>();
+  std::shared_ptr<ControllerBase> controller(new RateController);
   return controller;
 }
 
@@ -66,7 +66,7 @@ void RateController::CalculateRotorVelocities(Eigen::VectorXd* rotor_velocities)
   angular_acceleration_thrust(3) = control_rate_thrust_reference_(3);
 
   *rotor_velocities = angular_acc_to_rotor_velocities_ * angular_acceleration_thrust;
-  *rotor_velocities = rotor_velocities->cwiseMax(0);
+  *rotor_velocities = rotor_velocities->cwiseMax(Eigen::VectorXd::Zero(rotor_velocities->rows()));
   *rotor_velocities = rotor_velocities->cwiseSqrt();
 }
 
