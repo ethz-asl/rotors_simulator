0f88f5cec459469bfd2aea671f0251c7953846ce
Roman Bapst <romanbapst@yahoo.de>
2014-11-05 08:53:22 +0100

    added files supporting PX4 mutlicopter attiutde control

---
+++ mav_control/src/mc_mixer.cpp
@@ -0,0 +1,116 @@
+/*
+ * mc_mixer.cpp
+ *
+ *  Created on: Sep 17, 2014
+ *      Author: roman
+ */
+
+#include <mav_control/mc_mixer.h>
+#include <mathlib/mathlib.h>
+
+
+ // const MultirotorMixer::Rotor _config_quadshot[] =
+ // {
+ // { -0.3223,  0.9466,   0.4242 },
+ // {  0.3223, -0.9466,   1.0000 },
+ // {  0.3223,  0.9466,  -0.4242 },
+ // { -0.3223, -0.9466,  -1.0000 },
+ // };
+
+
+const MultirotorMixer::Rotor _config_quadshot[] = {
+	{  0.500000, -0.866025, -1.00 },
+	{  1.000000,  0.000000,  1.00 },
+	{  0.500000,  0.866025, -1.00 },
+	{ -0.500000,  0.866025,  1.00 },
+	{ -1.000000,  0.000000, -1.00 },
+	{ -0.500000, -0.866025,  1.00 },
+};
+
+	const MultirotorMixer::Rotor *_config_index = { &_config_quadshot[0]
+
+	};
+
+MultirotorMixer::MultirotorMixer() :
+		_rotor_count(6), _rotors(_config_index) {
+
+	memset(&inputs,0,sizeof(inputs));
+	memset(&outputs,0,sizeof(outputs));
+
+} 
+
+
+
+void MultirotorMixer::mix(const Eigen::Vector4d* desired_moments,Eigen::VectorXd& motor_commands) {
+	for (unsigned i = 0; i < 4; i++)
+	{
+		this->inputs.control[i] = (*desired_moments)(i);
+	}
+
+
+	float roll = math::constrain(inputs.control[0], -1.0f, 1.0f);
+	float pitch = math::constrain(inputs.control[1], -1.0f, 1.0f);
+	float yaw = math::constrain(inputs.control[2], -1.0f, 1.0f);
+	float thrust = math::constrain(inputs.control[3], 0.0f, 1.0f);
+	float min_out = 0.0f;
+	float max_out = 0.0f;
+
+	/* perform initial mix pass yielding unbounded outputs, ignore yaw */
+	for (unsigned i = 0; i < _rotor_count; i++) {
+		float out = roll * _rotors[i].roll_scale
+				+ pitch * _rotors[i].pitch_scale + thrust;
+
+		/* limit yaw if it causes outputs clipping */
+		if (out >= 0.0f && out < -yaw * _rotors[i].yaw_scale) {
+			yaw = -out / _rotors[i].yaw_scale;
+		}
+
+		/* calculate min and max output values */
+		if (out < min_out) {
+			min_out = out;
+		}
+		if (out > max_out) {
+			max_out = out;
+		}
+
+		outputs.control[i] = out;
+	}
+	/* scale down roll/pitch controls if some outputs are negative, don't add yaw, keep total thrust */
+	if (min_out < 0.0f) {
+		float scale_in = thrust / (thrust - min_out);
+
+		/* mix again with adjusted controls */
+		for (unsigned i = 0; i < _rotor_count; i++) {
+			outputs.control[i] = scale_in
+					* (roll * _rotors[i].roll_scale
+							+ pitch * _rotors[i].pitch_scale) + thrust;
+		}
+
+	} else {
+		/* roll/pitch mixed without limiting, add yaw control */
+		for (unsigned i = 0; i < _rotor_count; i++) {
+			outputs.control[i] += yaw * _rotors[i].yaw_scale;
+		}
+	}
+
+	/* scale down all outputs if some outputs are too large, reduce total thrust */
+	float scale_out;
+	if (max_out > 1.0f) {
+		scale_out = 1.0f / max_out;
+
+	} else {
+		scale_out = 1.0f;
+	}
+
+	/* scale outputs to range _idle_speed..1, and do final limiting */
+	for (unsigned i = 0; i < _rotor_count; i++) {
+		outputs.control[i] = math::constrain(outputs.control[i], 0.0f, 1.0f);
+		motor_commands(i) = outputs.control[i];
+	}
+
+	
+	
+
+
+}
+
---
+++ mav_control/src/px4_att_controller.cpp
@@ -0,0 +1,63 @@
+#include <mav_control/px4_att_controller.h>
+#include <iostream>
+#include <ros/ros.h>
+#include <gazebo/gazebo.hh>
+
+PX4_AttitudeController::PX4_AttitudeController() {
+  _PX4_base_att_controller = new MulticopterAttitudeControlBase();
+  _mc_mixer                = new MultirotorMixer();
+  time_last = ros::Time::now().toNSec() / 1e3;
+
+
+}
+
+PX4_AttitudeController::~PX4_AttitudeController() {
+}
+
+std::shared_ptr<ControllerBase> PX4_AttitudeController::Clone() {
+  std::shared_ptr<ControllerBase> controller(new PX4_AttitudeController);
+
+  return controller;
+}
+
+void PX4_AttitudeController::InitializeParams() {
+  
+  amount_rotors_ = 6;
+  initialized_params_ = true;
+  
+}
+
+void PX4_AttitudeController::CalculateRotorVelocities(Eigen::VectorXd* rotor_velocities) const {
+  rotor_velocities->resize(amount_rotors_);
+  
+  // first we need to update our data in the base class
+  _PX4_base_att_controller->set_attitude(attitude_);
+  _PX4_base_att_controller->set_attitude_reference(control_attitude_thrust_reference_);
+  _PX4_base_att_controller->set_attitude_rates(angular_rate_);
+   // now start control
+  _PX4_base_att_controller->control_attitude(time_since_last_call());        // first control attitude
+  _PX4_base_att_controller->control_attitude_rates(time_since_last_call());  // control attitude rates
+  _PX4_base_att_controller->set_actuator_controls();                         // write result to struct
+  
+  // get the actuator inputs from controller
+  Eigen::Vector4d mixer_input;
+  _PX4_base_att_controller->get_mixer_input(mixer_input);
+      
+  // now mix and save motor commands
+  Eigen::VectorXd mixer_output;
+  mixer_output.resize(amount_rotors_);
+  _mc_mixer->mix(&mixer_input,mixer_output);
+  *rotor_velocities = mixer_output * 1000;    // bring the data out
+}
+
+
+void PX4_AttitudeController::ComputeDesiredAngularAcc(Eigen::Vector3d* angular_acceleration) const {
+  
+}
+
+float PX4_AttitudeController::time_since_last_call() const {
+  float now = ros::Time::now().toNSec() / 1e3;
+  return (now - time_last) / 1e6;
+}
+
+MAV_CONTROL_REGISTER_CONTROLLER(PX4_AttitudeController);
