70f0e1db998259a851480657d51d486b34997174
Roman Bapst <romanbapst@yahoo.de>
2014-12-30 14:54:43 +0100

    cleanup

--- mav_control/include/mav_control/px4_dummy_controller.h
+++ mav_control/include/mav_control/px4_dummy_controller.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013, 2014 PX4 Development Team. All rights reserved.
+/* Copyright (c) 2014 PX4 Development Team. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions
@@ -30,11 +30,12 @@
 ****************************************************************************/
 
 /**
- * @file mc_att_control_main.cpp
- * Multicopter attitude controller.
+ * @file px4_dummy_controller.h
+ * Multicopter dummy controller to allow interface between euroc simulator and PX4
+ * apps running on ROS
  *
- * @author Roman Bapst <bapstr@student.ethz.ch>
- *  
+ * @author Roman Bapst <romanbapst@yahoo.de>
+ *
  */
 
 #include <mav_control/controller_base.h>
@@ -50,17 +51,17 @@ class PX4dummyController : public ControllerBase {
   virtual std::shared_ptr<ControllerBase> Clone();
   virtual void CalculateRotorVelocities(Eigen::VectorXd* rotor_velocities) const;
 
-  
+
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
  private:
- 	ros::NodeHandle _n;
- 	ros::Subscriber _sub;
+	ros::NodeHandle _n;
+	ros::Subscriber _sub;
 
- 	float _motor_speeds[4];
+	float _motor_speeds[4];
 
- 	void MotorVelCallback(const mav_msgs::MotorSpeed &msg);
+	void MotorVelCallback(const mav_msgs::MotorSpeed &msg);
 
- 	void ComputeDesiredAngularAcc(Eigen::Vector3d * angular_acceleration) const;
- 
+	void ComputeDesiredAngularAcc(Eigen::Vector3d * angular_acceleration) const;
+
 
 };
