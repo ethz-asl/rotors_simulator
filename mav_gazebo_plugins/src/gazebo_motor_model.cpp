8637374d5e1730073b52e5e70ac306c491793f1d
Fadri Furrer <ffurrer@gmail.com>
2015-01-13 14:22:25 +0100

    gazebo_motor_model: removed the newline character on CHECK error message

--- mav_gazebo_plugins/src/gazebo_motor_model.cpp
+++ mav_gazebo_plugins/src/gazebo_motor_model.cpp
@@ -128,7 +128,7 @@ void GazeboMotorModel::OnUpdate(const common::UpdateInfo& _info) {
 void GazeboMotorModel::VelocityCallback(const mav_msgs::MotorSpeedPtr& rot_velocities) {
   CHECK(rot_velocities->motor_speed.size() > motor_number_)
       << "You tried to access index " << motor_number_
-      << " of the MotorSpeed message array which is of size " << rot_velocities->motor_speed.size() << ".\n";
+      << " of the MotorSpeed message array which is of size " << rot_velocities->motor_speed.size() << ".";
   ref_motor_rot_vel_ = std::min(rot_velocities->motor_speed[motor_number_], static_cast<float>(max_rot_velocity_));
 }
 
