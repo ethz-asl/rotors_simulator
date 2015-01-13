01a1c289fd51034c8745e359430cd5b9785e123f
Fadri Furrer <ffurrer@gmail.com>
2015-01-13 19:39:51 +0100

    mav_control: fix include ordering

--- mav_control/include/mav_control/controller_factory.h
+++ mav_control/include/mav_control/controller_factory.h
@@ -5,19 +5,20 @@
  * Copyright (C) 2014 Sammy Omari, ASL, ETH Zurich, Switzerland
  * Copyright (C) 2014 Markus Achtelik, ASL, ETH Zurich, Switzerland
  *
- * This software is released to the Contestants of the european 
- * robotics challenges (EuRoC) for the use in stage 1. (Re)-distribution, whether 
- * in parts or entirely, is NOT PERMITTED. 
- * 
+ * This software is released to the Contestants of the european
+ * robotics challenges (EuRoC) for the use in stage 1. (Re)-distribution, whether
+ * in parts or entirely, is NOT PERMITTED.
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
 
 // controller_factory
-#include <string>
 #include <map>
 #include <memory>
+#include <string>
+
 #include <mav_control/controller_base.h>
 
 #define PP_CAT(a, b) a ## b
