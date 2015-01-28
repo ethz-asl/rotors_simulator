5836e5ea18f0fd5d165b64a6a9d994a002e20a54
Fadri Furrer <ffurrer@gmail.com>
2015-01-28 18:12:23 +0100

    gazebo_odometry_plugin: added an initial version of an odometry publisher
    This is untested and still has some TODOs so don't merge yet.

---
+++ mav_gazebo_plugins/include/mav_gazebo_plugins/gazebo_odometry_plugin.h
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2014 Fadri Furrer, ASL, ETH Zurich, Switzerland
+ * Copyright (C) 2014 Michael Burri, ASL, ETH Zurich, Switzerland
+ * Copyright (C) 2014 Pascal Gohl, ASL, ETH Zurich, Switzerland
+ * Copyright (C) 2014 Sammy Omari, ASL, ETH Zurich, Switzerland
+ * Copyright (C) 2014 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ *
+ * This software is released to the Contestants of the european
+ * robotics challenges (EuRoC) for the use in stage 1. (Re)-distribution, whether
+ * in parts or entirely, is NOT PERMITTED.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+
+#ifndef MAV_GAZEBO_PLUGINS_GAZEBO_ODOMETRY_PLUGIN_H
+#define MAV_GAZEBO_PLUGINS_GAZEBO_ODOMETRY_PLUGIN_H
+
+#include <cmath>
+#include <deque>
+#include <random>
+#include <stdio.h>
+
+#include <boost/bind.hpp>
+#include <gazebo/common/common.hh>
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <geometry_msgs/PoseWithCovariance.h>
+#include <geometry_msgs/TwistWithCovariance.h>
+#include <nav_msgs/Odometry.h>
+#include <opencv2/core/core.hpp>
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+
+namespace gazebo {
+// Default values
+static const std::string kDefaultNamespace = "";
+static const std::string kDefaultFrameId = "odometry_sensor_link";
+static const std::string kDefaultLinkName = "odometry_sensor_link";
+static const std::string kDefaultOdometryPubTopic = "odometry";
+
+static constexpr int kDefaultMeasurementDelay = 0;
+static constexpr int kDefaultMeasurementDivisor = 1;
+static constexpr int kDefaultGazeboSequence = 0;
+static constexpr int kDefaultOdometrySequence = 0;
+static constexpr double kDefaultUnknownDelay = 0.0;
+static constexpr double kDefaultCovarianceImageScale = 1.0;
+
+class GazeboOdometryPlugin : public ModelPlugin {
+ public:
+  typedef std::normal_distribution<> NormalDistribution;
+  typedef std::uniform_real_distribution<> UniformDistribution;
+  typedef std::deque<std::pair<int, nav_msgs::Odometry> > OdometryQueue;
+
+  GazeboOdometryPlugin()
+      : ModelPlugin(),
+        random_generator_(random_device_()),
+        odometry_pub_topic_(kDefaultOdometryPubTopic),
+        frame_id_(kDefaultFrameId),
+        link_name_(kDefaultLinkName),
+        measurement_delay_(kDefaultMeasurementDelay),
+        measurement_divisor_(kDefaultMeasurementDivisor),
+        unknown_delay_(kDefaultUnknownDelay),
+        gazebo_sequence_(kDefaultGazeboSequence),
+        odometry_sequence_(kDefaultOdometrySequence),
+        covariance_image_scale_(kDefaultCovarianceImageScale),
+        node_handle_(NULL) {}
+
+  ~GazeboOdometryPlugin();
+
+  void InitializeParams();
+  void Publish();
+
+ protected:
+  void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+  void OnUpdate(const common::UpdateInfo& /*_info*/);
+
+ private:
+  OdometryQueue odometry_queue_;
+
+  std::string namespace_;
+  std::string odometry_pub_topic_;
+  std::string frame_id_;
+  std::string link_name_;
+
+  NormalDistribution position_n_[3];
+  NormalDistribution attitude_n_[3];
+  NormalDistribution linear_n_[3];
+  NormalDistribution angular_n_[3];
+  UniformDistribution position_u_[3];
+  UniformDistribution attitude_u_[3];
+  UniformDistribution linear_u_[3];
+  UniformDistribution angular_u_[3];
+
+  geometry_msgs::PoseWithCovariance::_covariance_type pose_covariance_matrix_;
+  geometry_msgs::TwistWithCovariance::_covariance_type twist_covariance_matrix_;
+
+  int measurement_delay_;
+  int measurement_divisor_;
+  int gazebo_sequence_;
+  int odometry_sequence_;
+  double unknown_delay_;
+  double covariance_image_scale_;
+  cv::Mat covariance_image_;
+
+  std::random_device random_device_;
+  std::mt19937 random_generator_;
+
+  ros::NodeHandle* node_handle_;
+  ros::Publisher odometry_pub_;
+
+  physics::WorldPtr world_;
+  physics::ModelPtr model_;
+  physics::LinkPtr link_;
+
+  /// \brief Pointer to the update event connection.
+  event::ConnectionPtr updateConnection_;
+
+  boost::thread callback_queue_thread_;
+  void QueueThread();
+};
+}
+
+#endif // MAV_GAZEBO_PLUGINS_GAZEBO_ODOMETRY_PLUGIN_H
---
+++ mav_gazebo_plugins/src/gazebo_odometry_plugin.cpp
@@ -0,0 +1,265 @@
+/*
+ * Copyright (C) 2014 Fadri Furrer, ASL, ETH Zurich, Switzerland
+ * Copyright (C) 2014 Michael Burri, ASL, ETH Zurich, Switzerland
+ * Copyright (C) 2014 Pascal Gohl, ASL, ETH Zurich, Switzerland
+ * Copyright (C) 2014 Sammy Omari, ASL, ETH Zurich, Switzerland
+ * Copyright (C) 2014 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ *
+ * This software is released to the Contestants of the european
+ * robotics challenges (EuRoC) for the use in stage 1. (Re)-distribution, whether
+ * in parts or entirely, is NOT PERMITTED.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+
+#include <mav_gazebo_plugins/gazebo_odometry_plugin.h>
+
+#include <chrono>
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <mav_gazebo_plugins/common.h>
+
+namespace gazebo {
+
+GazeboOdometryPlugin::~GazeboOdometryPlugin() {
+  event::Events::DisconnectWorldUpdateBegin(updateConnection_);
+  if (node_handle_) {
+    node_handle_->shutdown();
+    delete node_handle_;
+  }
+}
+
+// void GazeboOdometryPlugin::InitializeParams() {};
+// void GazeboOdometryPlugin::Publish() {};
+
+void GazeboOdometryPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
+  // Store the pointer to the model
+  model_ = _model;
+  world_ = model_->GetWorld();
+
+  sdf::Vector3 noise_normal_position;
+  sdf::Vector3 noise_normal_quaternion;
+  sdf::Vector3 noise_normal_linear;
+  sdf::Vector3 noise_normal_angular;
+  sdf::Vector3 noise_uniform_position;
+  sdf::Vector3 noise_uniform_quaternion;
+  sdf::Vector3 noise_uniform_linear;
+  sdf::Vector3 noise_uniform_angular;
+  const sdf::Vector3 zeros3(0.0, 0.0, 0.0);
+
+  odometry_queue_.clear();
+
+  if (_sdf->HasElement("robotNamespace"))
+    namespace_ = _sdf->GetElement("robotNamespace")->Get<std::string>();
+  else
+    gzerr << "[gazebo_odometry_plugin] Please specify a robotNamespace.\n";
+  node_handle_ = new ros::NodeHandle(namespace_);
+
+  if (_sdf->HasElement("linkName"))
+    link_name_ = _sdf->GetElement("linkName")->Get<std::string>();
+  else
+    gzerr << "[gazebo_odometry_plugin] Please specify a linkName.\n";
+  link_ = model_->GetLink(link_name_);
+  if (link_ == NULL)
+    gzthrow("[gazebo_odometry_plugin] Couldn't find specified link \"" << link_name_ << "\".");
+
+
+
+  if (_sdf->HasElement("covarianceImage")) {
+    std::string image_name = _sdf->GetElement("covarianceImage")->Get<std::string>();
+    covariance_image_ = cv::imread(image_name, CV_LOAD_IMAGE_GRAYSCALE);
+    if (covariance_image_.data == NULL)
+      gzerr << "loading covariance image " << image_name << " failed" << std::endl;
+    else
+      gzlog << "loading covariance image " << image_name << " successful" << std::endl;
+  }
+
+  if (_sdf->HasElement("randomEngineSeed")) {
+    random_generator_.seed(_sdf->GetElement("randomEngineSeed")->Get<unsigned int>());
+  }
+  else {
+    random_generator_.seed(std::chrono::system_clock::now().time_since_epoch().count());
+  }
+
+  getSdfParam<std::string>(_sdf, "odometryTopic", odometry_pub_topic_, "odometry");
+  getSdfParam<std::string>(_sdf, "frameId", frame_id_, frame_id_);
+  getSdfParam<sdf::Vector3>(_sdf, "noiseNormalPosition", noise_normal_position, zeros3);
+  getSdfParam<sdf::Vector3>(_sdf, "noiseNormalQuaternion", noise_normal_quaternion, zeros3);
+  getSdfParam<sdf::Vector3>(_sdf, "noiseUniformPosition", noise_uniform_position, zeros3);
+  getSdfParam<sdf::Vector3>(_sdf, "noiseUniformQuaternion", noise_uniform_quaternion, zeros3);
+  getSdfParam<int>(_sdf, "measurementDelay", measurement_delay_, measurement_delay_);
+  getSdfParam<int>(_sdf, "measurementDivisor", measurement_divisor_, measurement_divisor_);
+  getSdfParam<double>(_sdf, "unknownDelay", unknown_delay_, unknown_delay_);
+  getSdfParam<double>(_sdf, "covarianceImageScale", covariance_image_scale_, covariance_image_scale_);
+
+  position_n_[0] = NormalDistribution(0, noise_normal_position.x);
+  position_n_[1] = NormalDistribution(0, noise_normal_position.y);
+  position_n_[2] = NormalDistribution(0, noise_normal_position.z);
+
+  attitude_n_[0] = NormalDistribution(0, noise_normal_quaternion.x);
+  attitude_n_[1] = NormalDistribution(0, noise_normal_quaternion.y);
+  attitude_n_[2] = NormalDistribution(0, noise_normal_quaternion.z);
+
+  position_u_[0] = UniformDistribution(-noise_uniform_position.x, noise_uniform_position.x);
+  position_u_[1] = UniformDistribution(-noise_uniform_position.y, noise_uniform_position.y);
+  position_u_[2] = UniformDistribution(-noise_uniform_position.z, noise_uniform_position.z);
+
+  attitude_u_[0] = UniformDistribution(-noise_uniform_quaternion.x, noise_uniform_quaternion.x);
+  attitude_u_[1] = UniformDistribution(-noise_uniform_quaternion.y, noise_uniform_quaternion.y);
+  attitude_u_[2] = UniformDistribution(-noise_uniform_quaternion.z, noise_uniform_quaternion.z);
+
+  // Fill in covariance. We omit uniform noise here.
+  Eigen::Map<Eigen::Matrix<double, 6, 6> > pose_covariance(pose_covariance_matrix_.data());
+  Eigen::Matrix<double, 6, 1> pose_covd;
+
+  pose_covd << noise_normal_position.x * noise_normal_position.x,
+               noise_normal_position.y * noise_normal_position.y,
+               noise_normal_position.z * noise_normal_position.z,
+               noise_normal_quaternion.x * noise_normal_quaternion.x,
+               noise_normal_quaternion.y * noise_normal_quaternion.y,
+               noise_normal_quaternion.z * noise_normal_quaternion.z;
+  pose_covariance = pose_covd.asDiagonal();
+
+  // Fill in covariance. We omit uniform noise here.
+  Eigen::Map<Eigen::Matrix<double, 6, 6> > twist_covariance(twist_covariance_matrix_.data());
+  Eigen::Matrix<double, 6, 1> twist_covd;
+
+  twist_covd << noise_normal_linear.x * noise_normal_linear.x,
+                noise_normal_linear.y * noise_normal_linear.y,
+                noise_normal_linear.z * noise_normal_linear.z,
+                noise_normal_angular.x * noise_normal_angular.x,
+                noise_normal_angular.y * noise_normal_angular.y,
+                noise_normal_angular.z * noise_normal_angular.z;
+  twist_covariance = twist_covd.asDiagonal();
+
+  frame_id_ = namespace_ + "/" + link_name_;
+
+  // Listen to the update event. This event is broadcast every
+  // simulation iteration.
+  updateConnection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&GazeboOdometryPlugin::OnUpdate, this, _1));
+
+  odometry_pub_ = node_handle_->advertise<nav_msgs::Odometry>(odometry_pub_topic_, 10);
+}
+
+// This gets called by the world update start event.
+void GazeboOdometryPlugin::OnUpdate(const common::UpdateInfo& _info) {
+  math::Pose gazebo_pose = link_->GetWorldCoGPose();
+  math::Vector3 gazebo_linear_velocity = link_->GetRelativeLinearVel();
+  math::Vector3 gazebo_angular_velocity = link_->GetRelativeAngularVel();
+  bool publish_odometry = true;
+
+  // First, determine whether we should publish a odometry.
+  if (covariance_image_.data != NULL) {
+    // We have an image.
+
+    // Image is always centered around the origin:
+    int width = covariance_image_.cols;
+    int height = covariance_image_.rows;
+    int x = static_cast<int>(std::floor(gazebo_pose.pos.x / covariance_image_scale_)) + width / 2;
+    int y = static_cast<int>(std::floor(gazebo_pose.pos.y / covariance_image_scale_)) + height / 2;
+
+    if (x >= 0 && x < width && y >= 0 && y < height) {
+      uint8_t val = covariance_image_.at<uint8_t>(y, x);
+      if (val == 0)
+        publish_odometry = false;
+      // TODO: covariance scaling, according to the intensity values could be implemented here.
+    }
+  }
+
+  if (gazebo_sequence_ % measurement_divisor_ == 0) {
+    nav_msgs::Odometry odometry;
+    odometry.header.frame_id = frame_id_;
+    odometry.header.seq = odometry_sequence_++;
+    odometry.header.stamp.sec = (world_->GetSimTime()).sec + ros::Duration(unknown_delay_).sec;
+    odometry.header.stamp.nsec = (world_->GetSimTime()).nsec + ros::Duration(unknown_delay_).nsec;
+    odometry.child_frame_id = frame_id_;
+    copyPosition(gazebo_pose.pos, &odometry.pose.pose.position);
+    odometry.pose.pose.orientation.w = gazebo_pose.rot.w;
+    odometry.pose.pose.orientation.x = gazebo_pose.rot.x;
+    odometry.pose.pose.orientation.y = gazebo_pose.rot.y;
+    odometry.pose.pose.orientation.z = gazebo_pose.rot.z;
+    odometry.twist.twist.linear.x = gazebo_linear_velocity.x;
+    odometry.twist.twist.linear.y = gazebo_linear_velocity.y;
+    odometry.twist.twist.linear.z = gazebo_linear_velocity.z;
+    odometry.twist.twist.angular.x = gazebo_angular_velocity.x;
+    odometry.twist.twist.angular.y = gazebo_angular_velocity.y;
+    odometry.twist.twist.angular.z = gazebo_angular_velocity.z;
+
+    if (publish_odometry)
+      odometry_queue_.push_back(std::make_pair(gazebo_sequence_ + measurement_delay_, odometry));
+  }
+
+  // Is it time to publish the front element?
+  if (gazebo_sequence_ == odometry_queue_.front().first) {
+    nav_msgs::OdometryPtr odometry(new nav_msgs::Odometry);
+
+    odometry->header = odometry_queue_.front().second.header;
+    odometry->pose.pose = odometry_queue_.front().second.pose.pose;
+    odometry->twist.twist.linear = odometry_queue_.front().second.twist.twist.linear;
+    odometry->twist.twist.angular = odometry_queue_.front().second.twist.twist.angular;
+    odometry_queue_.pop_front();
+
+    // Calculate position distortions.
+    Eigen::Vector3d pos_n;
+    pos_n << position_n_[0](random_generator_) + position_u_[0](random_generator_),
+             position_n_[1](random_generator_) + position_u_[1](random_generator_),
+             position_n_[2](random_generator_) + position_u_[2](random_generator_);
+    geometry_msgs::Point& p = odometry->pose.pose.position;
+    p.x += pos_n[0];
+    p.y += pos_n[1];
+    p.z += pos_n[2];
+
+    // Calculate attitude distortions.
+    Eigen::Vector3d theta;
+    theta << attitude_n_[0](random_generator_) + attitude_u_[0](random_generator_),
+             attitude_n_[1](random_generator_) + attitude_u_[1](random_generator_),
+             attitude_n_[2](random_generator_) + attitude_u_[2](random_generator_);
+    Eigen::Quaterniond q_n = QuaternionFromSmallAngle(theta);
+    q_n.normalize();
+    geometry_msgs::Quaternion& q = odometry->pose.pose.orientation;
+    Eigen::Quaterniond _q(q.w, q.x, q.y, q.z);
+    _q = _q * q_n;
+    q.w = _q.w();
+    q.x = _q.x();
+    q.y = _q.y();
+    q.z = _q.z();
+
+    // Calculate linear velocity distortions.
+    Eigen::Vector3d linear_n;
+    linear_n << linear_n_[0](random_generator_) + linear_u_[0](random_generator_),
+                linear_n_[1](random_generator_) + linear_u_[1](random_generator_),
+                linear_n_[2](random_generator_) + linear_u_[2](random_generator_);
+    geometry_msgs::Vector3& linear_velocity = odometry->twist.twist.linear;
+    linear_velocity.x += linear_n[0];
+    linear_velocity.y += linear_n[1];
+    linear_velocity.z += linear_n[2];
+
+    // Calculate angular veocity distortions.
+    Eigen::Vector3d angular_n;
+    angular_n << angular_n_[0](random_generator_) + angular_u_[0](random_generator_),
+                angular_n_[1](random_generator_) + angular_u_[1](random_generator_),
+                angular_n_[2](random_generator_) + angular_u_[2](random_generator_);
+    geometry_msgs::Vector3& angular_velocity = odometry->twist.twist.angular;
+    angular_velocity.x += angular_n[0];
+    angular_velocity.y += angular_n[1];
+    angular_velocity.z += angular_n[2];
+
+    odometry->pose.covariance = pose_covariance_matrix_; //TODO(ff): Put the proper covariance.
+    odometry->twist.covariance = twist_covariance_matrix_; //TODO(ff): Put the proper covariance.
+
+    odometry_pub_.publish(odometry);
+//    std::cout << "published odometry with timestamp " << odometry->header.stamp << "at time t" << world_->GetSimTime().Double()
+//        << "delay should be " << measurement_delay_ << "sim cycles" << std::endl;
+  }
+
+  ++gazebo_sequence_;
+}
+
+GZ_REGISTER_MODEL_PLUGIN(GazeboOdometryPlugin);
+}
