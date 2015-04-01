71c1ddef1ff42345090a2dd8e112683ecf7ac46f
Mina Kamel <fmina@ethz.ch>
2015-04-01 11:34:46 +0200

    Fix yaw angle
    Apply torques on the main mav link

--- rotors_gazebo_plugins/src/gazebo_motor_model.cpp
+++ rotors_gazebo_plugins/src/gazebo_motor_model.cpp
@@ -150,13 +150,14 @@ void GazeboMotorModel::UpdateForcesAndMoments() {
   math::Vector3 air_drag = -std::abs(real_motor_velocity) * rotor_drag_coefficient_ * body_velocity_perpendicular;
   // Apply air_drag to link.
   link_->AddForce(air_drag);
-  // Moments
-  link_->AddRelativeTorque(math::Vector3(0, 0, -turning_direction_ * force * moment_constant_));
+  // Moments (fmina: applied to the main link)
+  physics::Link_V parent_links = link_->GetParentJointsLinks();
+  parent_links.at(0)->AddRelativeTorque(math::Vector3(0, 0, -turning_direction_ * force * moment_constant_));
 
   math::Vector3 rolling_moment;
   // - \omega * \mu_1 * V_A^{\perp}
   rolling_moment = -std::abs(real_motor_velocity) * rolling_moment_coefficient_ * body_velocity_perpendicular;
-  link_->AddRelativeTorque(rolling_moment);
+  parent_links.at(0)->AddRelativeTorque(rolling_moment);
   // Apply the filter on the motor's velocity.
   ref_motor_rot_vel_ = rotor_velocity_filter_->updateFilter(ref_motor_rot_vel_, sampling_time_);
   joint_->SetVelocity(0, turning_direction_ * ref_motor_rot_vel_ / rotor_velocity_slowdown_sim_);
