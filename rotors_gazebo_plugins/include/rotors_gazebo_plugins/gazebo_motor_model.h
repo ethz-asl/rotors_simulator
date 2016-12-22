671903b0a02cb642d98990768002b5f8f6dab423
Geoffrey Hunter <gbmhunter@gmail.com>
2016-12-22 02:40:52 -0800

    Removed un-used headers from gazebo_motor_model.h.

--- rotors_gazebo_plugins/include/rotors_gazebo_plugins/gazebo_motor_model.h
+++ rotors_gazebo_plugins/include/rotors_gazebo_plugins/gazebo_motor_model.h
@@ -30,12 +30,12 @@
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
-#include <mav_msgs/Actuators.h>
-#include <mav_msgs/default_topics.h>
-#include <ros/callback_queue.h>
-#include <ros/ros.h>
-#include <rotors_comm/WindSpeed.h>
-#include <std_msgs/Float32.h>
+//#include <mav_msgs/Actuators.h>
+//#include <mav_msgs/default_topics.h>
+//#include <ros/callback_queue.h>
+//#include <ros/ros.h>
+//#include <rotors_comm/WindSpeed.h>
+//#include <std_msgs/Float32.h>
 
 #include "rotors_gazebo_plugins/common.h"
 #include "rotors_gazebo_plugins/motor_model.hpp"
