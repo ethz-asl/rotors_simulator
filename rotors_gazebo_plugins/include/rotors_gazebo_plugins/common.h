85c6d1c936eb7c72bb98307b0590e3fe5c5aa328
Geoffrey Hunter <gbmhunter@gmail.com>
2017-01-19 04:27:36 -0800

    Tidied up comments in 'common.h'.

--- rotors_gazebo_plugins/include/rotors_gazebo_plugins/common.h
+++ rotors_gazebo_plugins/include/rotors_gazebo_plugins/common.h
@@ -56,14 +56,13 @@ static constexpr double kDefaultRotorVelocitySlowdownSim = 10.0;
 static const std::string kConnectGazeboToRosSubtopic = "connect_gazebo_to_ros_subtopic";
 static const std::string kConnectRosToGazeboSubtopic = "connect_ros_to_gazebo_subtopic";
 
-/**
- * \brief Obtains a parameter from sdf.
- * \param[in] sdf Pointer to the sdf object.
- * \param[in] name Name of the parameter.
- * \param[out] param Param Variable to write the parameter to.
- * \param[in] default_value Default value, if the parameter not available.
- * \param[in] verbose If true, gzerror if the parameter is not available.
- */
+
+/// \brief      Obtains a parameter from sdf.
+/// \param[in]  sdf           Pointer to the sdf object.
+/// \param[in]  name          Name of the parameter.
+/// \param[out] param         Param Variable to write the parameter to.
+/// \param[in]  default_value Default value, if the parameter not available.
+/// \param[in]  verbose       If true, gzerror if the parameter is not available.
 template<class T>
 bool getSdfParam(sdf::ElementPtr sdf, const std::string& name, T& param, const T& default_value, const bool& verbose =
                      false) {
@@ -81,57 +80,55 @@ bool getSdfParam(sdf::ElementPtr sdf, const std::string& name, T& param, const T
 
 }
 
+/// \brief    This class can be used to apply a first order filter on a signal.
+///           It allows different acceleration and deceleration time constants.
+/// \details
+///           Short reveiw of discrete time implementation of first order system:
+///           Laplace:
+///             X(s)/U(s) = 1/(tau*s + 1)
+///           continous time system:
+///             dx(t) = (-1/tau)*x(t) + (1/tau)*u(t)
+///           discretized system (ZoH):
+///             x(k+1) = exp(samplingTime*(-1/tau))*x(k) + (1 - exp(samplingTime*(-1/tau))) * u(k)
 template <typename T>
 class FirstOrderFilter {
-/*
-This class can be used to apply a first order filter on a signal.
-It allows different acceleration and deceleration time constants.
 
-Short reveiw of discrete time implementation of first order system:
-Laplace:
-    X(s)/U(s) = 1/(tau*s + 1)
-continous time system:
-    dx(t) = (-1/tau)*x(t) + (1/tau)*u(t)
-discretized system (ZoH):
-    x(k+1) = exp(samplingTime*(-1/tau))*x(k) + (1 - exp(samplingTime*(-1/tau))) * u(k)
-*/
-
-  public:
-    FirstOrderFilter(double timeConstantUp, double timeConstantDown, T initialState):
+ public:
+  FirstOrderFilter(double timeConstantUp, double timeConstantDown, T initialState):
       timeConstantUp_(timeConstantUp),
       timeConstantDown_(timeConstantDown),
       previousState_(initialState) {}
 
-    T updateFilter(T inputState, double samplingTime) {
-      /*
-      This method will apply a first order filter on the inputState.
-      */
-      T outputState;
-      if (inputState > previousState_) {
-        // Calcuate the outputState if accelerating.
-        double alphaUp = exp(-samplingTime / timeConstantUp_);
-        // x(k+1) = Ad*x(k) + Bd*u(k)
-        outputState = alphaUp * previousState_ + (1 - alphaUp) * inputState;
+  /// \brief    This method will apply a first order filter on the inputState.
+  T updateFilter(T inputState, double samplingTime) {
 
-      }
-      else {
-        // Calculate the outputState if decelerating.
-        double alphaDown = exp(-samplingTime / timeConstantDown_);
-        outputState = alphaDown * previousState_ + (1 - alphaDown) * inputState;
-      }
-      previousState_ = outputState;
-      return outputState;
+    T outputState;
+    if (inputState > previousState_) {
+      // Calcuate the outputState if accelerating.
+      double alphaUp = exp(-samplingTime / timeConstantUp_);
+      // x(k+1) = Ad*x(k) + Bd*u(k)
+      outputState = alphaUp * previousState_ + (1 - alphaUp) * inputState;
 
     }
-    ~FirstOrderFilter() {}
+    else {
+      // Calculate the outputState if decelerating.
+      double alphaDown = exp(-samplingTime / timeConstantDown_);
+      outputState = alphaDown * previousState_ + (1 - alphaDown) * inputState;
+    }
+    previousState_ = outputState;
+    return outputState;
 
-  protected:
-    double timeConstantUp_;
-    double timeConstantDown_;
-    T previousState_;
+  }
+
+  ~FirstOrderFilter() {}
+
+ protected:
+  double timeConstantUp_;
+  double timeConstantDown_;
+  T previousState_;
 };
 
-/// Computes a quaternion from the 3-element small angle approximation theta.
+/// \brief    Computes a quaternion from the 3-element small angle approximation theta.
 template<class Derived>
 Eigen::Quaternion<typename Derived::Scalar> QuaternionFromSmallAngle(const Eigen::MatrixBase<Derived> & theta) {
   typedef typename Derived::Scalar Scalar;
