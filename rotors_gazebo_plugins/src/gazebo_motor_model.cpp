b3ab794323ced42e2a61b802f8586987bd28d520
Geoffrey Hunter <gbmhunter@gmail.com>
2017-02-10 14:43:59 -0800

    Gazebo namespace is now required for all ROS to Gazebo messages. All plugins should be now setting this correctly.

--- rotors_gazebo_plugins/src/gazebo_motor_model.cpp
+++ rotors_gazebo_plugins/src/gazebo_motor_model.cpp
@@ -176,6 +176,7 @@ void GazeboMotorModel::CreatePubsAndSubs() {
   command_sub_ = node_handle_->Subscribe("~/" + model_->GetName() + "/" + command_sub_topic_, &GazeboMotorModel::ControlVelocityCallback, this);
 
   connect_ros_to_gazebo_topic_msg.set_ros_topic(command_sub_topic_);
+  connect_ros_to_gazebo_topic_msg.set_gazebo_namespace(namespace_);
   connect_ros_to_gazebo_topic_msg.set_gazebo_topic("~/" + model_->GetName() + "/" + command_sub_topic_);
   connect_ros_to_gazebo_topic_msg.set_msgtype(gz_std_msgs::ConnectRosToGazeboTopic::COMMAND_MOTOR_SPEED);
   gz_connect_ros_to_gazebo_topic_pub->Publish(connect_ros_to_gazebo_topic_msg, true);
@@ -187,6 +188,7 @@ void GazeboMotorModel::CreatePubsAndSubs() {
   wind_speed_sub_ = node_handle_->Subscribe("~/" + model_->GetName() + "/" + wind_speed_sub_topic_, &GazeboMotorModel::WindSpeedCallback, this);
 
   connect_ros_to_gazebo_topic_msg.set_ros_topic(wind_speed_sub_topic_);
+  connect_ros_to_gazebo_topic_msg.set_gazebo_namespace(namespace_);
   connect_ros_to_gazebo_topic_msg.set_gazebo_topic("~/" + model_->GetName() + "/" + wind_speed_sub_topic_);
   connect_ros_to_gazebo_topic_msg.set_msgtype(gz_std_msgs::ConnectRosToGazeboTopic::WIND_SPEED);
   gz_connect_ros_to_gazebo_topic_pub->Publish(connect_ros_to_gazebo_topic_msg, true);
