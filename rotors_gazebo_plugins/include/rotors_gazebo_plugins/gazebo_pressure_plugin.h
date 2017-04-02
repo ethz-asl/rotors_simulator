4cc8c159c6595a548b949b1feeb7a2174f484f67
pvechersky <pvechersky@gmail.com>
2017-04-02 22:49:57 +1000

    Adapting the pressure plugin to work with Gazebo transport interface

--- rotors_gazebo_plugins/include/rotors_gazebo_plugins/gazebo_pressure_plugin.h
+++ rotors_gazebo_plugins/include/rotors_gazebo_plugins/gazebo_pressure_plugin.h
@@ -1,9 +1,5 @@
 /*
- * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Mina Kamel, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Janosch Nikolic, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ * Copyright 2016 Pavel Vechersky, ASL, ETH Zurich, Switzerland
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,14 +17,12 @@
 #ifndef ROTORS_GAZEBO_PLUGINS_PRESSURE_PLUGIN_H
 #define ROTORS_GAZEBO_PLUGINS_PRESSURE_PLUGIN_H
 
-#include <Eigen/Core>
-#include <gazebo/common/common.hh>
 #include <gazebo/common/Plugin.hh>
+#include <gazebo/common/common.hh>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
-#include <mav_msgs/default_topics.h>
-#include <ros/ros.h>
-#include <sensor_msgs/FluidPressure.h>
+
+#include "FluidPressure.pb.h"
 
 #include "rotors_gazebo_plugins/common.h"
 
@@ -50,35 +44,68 @@ static constexpr double kDefaultPressureVar = 0.0; /* Pa^2, pressure variance */
 
 class GazeboPressurePlugin : public ModelPlugin {
  public:
+  /// \brief    Constructor.
   GazeboPressurePlugin();
+
+  /// \brief    Destructor.
   virtual ~GazeboPressurePlugin();
 
  protected:
+  /// \brief    Called when the plugin is first created, and after the world
+  ///           has been loaded. This function should not be blocking.
   void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+
+  /// \brief  	This gets called by the world update start event.
   void OnUpdate(const common::UpdateInfo&);
 
  private:
+  /// \brief    Flag that is set to true once CreatePubsAndSubs() is called, used
+  ///           to prevent CreatePubsAndSubs() from be called on every OnUpdate().
+  bool pubs_and_subs_created_;
+
+  /// \brief    Creates all required publishers and subscribers, incl. routing of messages to/from ROS if required.
+  /// \details  Call this once the first time OnUpdate() is called (can't
+  ///           be called from Load() because there is no guarantee GazeboRosInterfacePlugin has
+  ///           has loaded and listening to ConnectGazeboToRosTopic and ConnectRosToGazeboTopic messages).
+  void CreatePubsAndSubs();
+
+  /// \brief    Handle for the Gazebo node.
+  gazebo::transport::NodePtr node_handle_;
+
+  /// \brief    Pressure message publisher.
+  gazebo::transport::PublisherPtr pressure_pub_;
+
+  /// \brief    Transport namespace.
   std::string namespace_;
+
+  /// \brief    Topic name for pressure messages.
   std::string pressure_topic_;
-  ros::NodeHandle* node_handle_;
-  ros::Publisher pressure_pub_;
+
+  /// \brief    Frame ID for pressure messages.
   std::string frame_id_;
 
-  // Pointer to the world
+  /// \brief    Pointer to the world.
   physics::WorldPtr world_;
-  // Pointer to the model
+
+  /// \brief    Pointer to the model.
   physics::ModelPtr model_;
-  // Pointer to the link
+
+  /// \brief    Pointer to the link.
   physics::LinkPtr link_;
-  // Pointer to the update event connection
+
+  /// \brief    Pointer to the update event connection.
   event::ConnectionPtr updateConnection_;
 
-  int pressure_sequence_;
-
+  /// \brief    Reference altitude (meters).
   double ref_alt_;
+
+  /// \brief    Pressure measurement variance (Pa^2).
   double pressure_var_;
 
-  sensor_msgs::FluidPressure pressure_message_;
+  /// \brief    Fluid pressure message.
+  /// \details  This is modified everytime OnUpdate() is called,
+  //            and then published onto a topic
+  gz_sensor_msgs::FluidPressure pressure_message_;
 };
 }
 
--- rotors_gazebo_plugins/src/gazebo_pressure_plugin.cpp
+++ rotors_gazebo_plugins/src/gazebo_pressure_plugin.cpp
@@ -1,9 +1,5 @@
 /*
- * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Mina Kamel, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Janosch Nikolic, ASL, ETH Zurich, Switzerland
- * Copyright 2015 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ * Copyright 2016 Pavel Vechersky, ASL, ETH Zurich, Switzerland
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,96 +14,138 @@
  * limitations under the License.
  */
 
+// MODULE HEADER
 #include "rotors_gazebo_plugins/gazebo_pressure_plugin.h"
 
-#include <chrono>
-#include <cmath>
-#include <iostream>
-#include <stdio.h>
-
-#include <boost/bind.hpp>
+// USER HEADERS
+#include "ConnectGazeboToRosTopic.pb.h"
 
 namespace gazebo {
 
 GazeboPressurePlugin::GazeboPressurePlugin()
     : ModelPlugin(),
       node_handle_(0),
-      pressure_sequence_(0) {
+      pubs_and_subs_created_(false) {
 }
 
 GazeboPressurePlugin::~GazeboPressurePlugin() {
   event::Events::DisconnectWorldUpdateBegin(updateConnection_);
-  if (node_handle_) {
-    node_handle_->shutdown();
-    delete node_handle_;
-  }
 }
 
 void GazeboPressurePlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
-  // Store the pointer to the model and the world
+  if (kPrintOnPluginLoad) {
+    gzdbg << __FUNCTION__ << "() called." << std::endl;
+  }
+
+  gzdbg << "_model = " << _model->GetName() << std::endl;
+
+  // Store the pointer to the model and the world.
   model_ = _model;
   world_ = model_->GetWorld();
 
-  // Use the robot namespace to create the node handle
+  //==============================================//
+  //========== READ IN PARAMS FROM SDF ===========//
+  //==============================================//
+
+  // Use the robot namespace to create the node handle.
   if (_sdf->HasElement("robotNamespace"))
     namespace_ = _sdf->GetElement("robotNamespace")->Get<std::string>();
   else
     gzerr << "[gazebo_pressure_plugin] Please specify a robotNamespace.\n";
-  node_handle_ = new ros::NodeHandle(namespace_);
 
-  // Use the link name as the frame id
+  // Get node handle.
+  node_handle_ = transport::NodePtr(new transport::Node());
+
+  // Initialise with default namespace (typically /gazebo/default/).
+  node_handle_->Init();
+
   std::string link_name;
   if (_sdf->HasElement("linkName"))
     link_name = _sdf->GetElement("linkName")->Get<std::string>();
   else
     gzerr << "[gazebo_pressure_plugin] Please specify a linkName.\n";
-  // Get the pointer to the link
+  // Get the pointer to the link.
   link_ = model_->GetLink(link_name);
   if (link_ == NULL)
     gzthrow("[gazebo_pressure_plugin] Couldn't find specified link \"" << link_name << "\".");
 
   frame_id_ = link_name;
 
-  // Retrieve the rest of the SDF parameters
+  // Retrieve the rest of the SDF parameters.
   getSdfParam<std::string>(_sdf, "pressureTopic", pressure_topic_, kDefaultPressurePubTopic);
   getSdfParam<double>(_sdf, "referenceAltitude", ref_alt_, kDefaultRefAlt);
   getSdfParam<double>(_sdf, "pressureVariance", pressure_var_, kDefaultPressureVar);
 
-  // Listen to the update event. This event is broadcast every simulation iteration
-  this->updateConnection_ =
-      event::Events::ConnectWorldUpdateBegin(
-          boost::bind(&GazeboPressurePlugin::OnUpdate, this, _1));
+  // Listen to the update event. This event is broadcast every simulation
+  // iteration.
+  this->updateConnection_ = event::Events::ConnectWorldUpdateBegin(
+      boost::bind(&GazeboPressurePlugin::OnUpdate, this, _1));
 
-  pressure_pub_ = node_handle_->advertise<sensor_msgs::FluidPressure>(pressure_topic_, 1);
+  //==============================================//
+  //=== POPULATE STATIC PARTS OF PRESSURE MSG ====//
+  //==============================================//
 
-  // Fill the pressure message
-  pressure_message_.header.frame_id = frame_id_;
-  pressure_message_.variance = pressure_var_;
+  pressure_message_.mutable_header()->set_frame_id(frame_id_);
+  pressure_message_.set_variance(pressure_var_);
 }
 
 void GazeboPressurePlugin::OnUpdate(const common::UpdateInfo& _info) {
-  common::Time current_time  = world_->GetSimTime();
+  if (kPrintOnUpdates) {
+    gzdbg << __FUNCTION__ << "() called." << std::endl;
+  }
 
-  // Compute the geopotential altitude
+  if (!pubs_and_subs_created_) {
+    CreatePubsAndSubs();
+    pubs_and_subs_created_ = true;
+  }
+
+  common::Time current_time = world_->GetSimTime();
+
+  // Compute the geopotential altitude.
   double z = ref_alt_ + model_->GetWorldPose().pos.z;
   double h = kR0 * z / (kR0 + z);
 
-  // Compute the temperature at the current altitude
+  // Compute the temperature at the current altitude.
   double t = kT0 - kTl * h;
 
-  // Compute the current air pressure
+  // Compute the current air pressure.
   double p = kP0 * exp(kAs * log(kT0 / t));
 
-  // Fill the pressure message
-  pressure_message_.fluid_pressure = p;
-  pressure_message_.header.seq = pressure_sequence_;
-  pressure_message_.header.stamp.sec = current_time.sec;
-  pressure_message_.header.stamp.nsec = current_time.nsec;
+  // Fill the pressure message.
+  pressure_message_.mutable_header()->mutable_stamp()->set_sec(
+      current_time.sec);
+  pressure_message_.mutable_header()->mutable_stamp()->set_nsec(
+      current_time.nsec);
+  pressure_message_.set_fluid_pressure(p);
 
-  // Publish the message
-  pressure_pub_.publish(pressure_message_);
-  pressure_sequence_++;
+  // Publish the pressure message.
+  pressure_pub_->Publish(pressure_message_);
+}
+
+void GazeboPressurePlugin::CreatePubsAndSubs() {
+  // Create temporary "ConnectGazeboToRosTopic" publisher and message.
+  gazebo::transport::PublisherPtr connect_gazebo_to_ros_topic_pub =
+      node_handle_->Advertise<gz_std_msgs::ConnectGazeboToRosTopic>(
+          "~/" + kConnectGazeboToRosSubtopic, 1);
+
+  // ============================================ //
+  // ========= FLUID PRESSURE MSG SETUP ========= //
+  // ============================================ //
+
+  pressure_pub_ = node_handle_->Advertise<gz_sensor_msgs::FluidPressure>(
+      "~/" + namespace_ + "/" + pressure_topic_, 1);
+
+  gz_std_msgs::ConnectGazeboToRosTopic connect_gazebo_to_ros_topic_msg;
+  connect_gazebo_to_ros_topic_msg.set_gazebo_topic("~/" + namespace_ + "/" +
+                                                   pressure_topic_);
+  connect_gazebo_to_ros_topic_msg.set_ros_topic(namespace_ + "/" +
+                                                pressure_topic_);
+  connect_gazebo_to_ros_topic_msg.set_msgtype(
+      gz_std_msgs::ConnectGazeboToRosTopic::FLUID_PRESSURE);
+  connect_gazebo_to_ros_topic_pub->Publish(connect_gazebo_to_ros_topic_msg,
+                                           true);
 }
 
 GZ_REGISTER_MODEL_PLUGIN(GazeboPressurePlugin);
-}
+
+}  // namespace gazebo
