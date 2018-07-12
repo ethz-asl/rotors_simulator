59a6b2baca4f1ebbaf5001021d4f42bf7bf6751e
kajabo <karen.bodie@gmail.com>
2018-07-12 16:06:23 +0200

    Add default as former function (old stuff shouldn't break)

--- rotors_gazebo_plugins/src/gazebo_mavlink_interface.cpp
+++ rotors_gazebo_plugins/src/gazebo_mavlink_interface.cpp
@@ -951,20 +951,19 @@ void GazeboMavlinkInterface::handle_message(mavlink_message_t *msg)
     if ((controls.mode & MAV_MODE_FLAG_SAFETY_ARMED) > 0) {
       armed = true;
     }
+
     last_actuator_time_ = world_->GetSimTime();
 
     for (unsigned i = 0; i < kNOutMax; i++) {
       input_index_[i] = i;
     }
 
-    // set rotor speeds, controller targets
+    // Interpret flag 1 as rotor speeds, controller targets for VoliroX system (12 commands).
     if (controls.flags == 1) {
       input_reference_.resize(kNOutMax);
       for (int i = 0; i < 12; i++) {
         if (armed) {
           input_reference_[i] = (controls.controls[input_index_[i]] + input_offset_[i]) * input_scaling_[i] + zero_position_armed_[i];
-          // if (joints_[i])
-          //   gzerr << i << " : " << input_index_[i] << " : " << controls.controls[input_index_[i]] << " : " << input_reference_[i] << "\n";
         } else {
           input_reference_[i] = zero_position_disarmed_[i];
           }
@@ -972,16 +971,29 @@ void GazeboMavlinkInterface::handle_message(mavlink_message_t *msg)
       received_first_reference_ = true;
     }
 
-    // Receiving dynamixel angle message
-    if (controls.flags == 2) {
+    // Interpret flag 2 as a servo position message for voliroX system (indices 12 - 17).
+    else if (controls.flags == 2) {
       if(armed) {
         for (int i = 12; i < 18; i++) {
           input_reference_[i] = (controls.controls[input_index_[i-12]] + input_offset_[i])
               * input_scaling_[i] + zero_position_armed_[i];
         }
-        //gzdbg << "Dynamixel reference: " << input_reference_[12] << ", " << input_reference_[13] << ", " << input_reference_[14] << ", " << input_reference_[15] << ", " << input_reference_[16] << ", " << input_reference_[17] << std::endl;
       }
     }
+
+    // Set rotor speeds, controller targets for standard system.
+    else{
+      input_reference_.resize(kNOutMax);
+      for (int i = 0; i < kNOutMax; i++) {
+        if (armed) {
+          input_reference_[i] = (controls.controls[input_index_[i]] + input_offset_[i]) * input_scaling_[i] + zero_position_armed_[i];
+            } else {
+          input_reference_[i] = zero_position_disarmed_[i];
+          }
+      }
+      received_first_reference_ = true;
+    }
+
     break;
   }
 }
