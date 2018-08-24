1dcb7839631d33041961ef0f17e86d015e4446f5
Jubeen Lee <ljb7977@gmail.com>
2018-08-24 11:00:27 +0900

    terabee implemented.To run: $ rosrun rotors_gazebo_plugins terabee_managertopic name: /niv1/lidar_terabee

---
+++ rotors_gazebo_plugins/include/rotors_gazebo_plugins/external/gazebo_lidar_terabee_plugin.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2012-2014 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+/*
+ * Desc: Ray Plugin
+ * Author: Nate Koenig mod by John Hsu
+ */
+
+#ifndef _GAZEBO_LIDAR_TERABEE_PLUGIN_HH_
+#define _GAZEBO_LIDAR_TERABEE_PLUGIN_HH_
+
+#include "gazebo/common/Plugin.hh"
+#include "gazebo/sensors/SensorTypes.hh"
+#include "gazebo/sensors/RaySensor.hh"
+#include "gazebo/util/system.hh"
+
+#include "Lidar.pb.h"
+
+#include <mav_msgs/default_topics.h>
+#include <ros/callback_queue.h>
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include "std_msgs/String.h"
+
+namespace gazebo
+{
+/// \brief    A Gazebo LIDAR plugin.
+class GAZEBO_VISIBLE GazeboLidarTerabeePlugin : public SensorPlugin
+{
+  /// \brief Constructor
+public:
+  GazeboLidarTerabeePlugin();
+
+  /// \brief Destructor
+public:
+  virtual ~GazeboLidarTerabeePlugin();
+
+  /// \brief Update callback
+public:
+  virtual void OnNewLaserScans();
+
+  /// \brief Load the plugin
+  /// \param take in SDF root element
+public:
+  void Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf);
+
+  /// \brief Pointer to parent
+protected:
+  physics::WorldPtr world;
+
+  /// \brief The parent sensor
+private:
+  bool pubs_and_subs_created_;
+  /// \brief    Creates all required publishers and subscribers, incl. routing of messages to/from ROS if required.
+  /// \details  Call this once the first time OnUpdate() is called (can't
+  ///           be called from Load() because there is no guarantee GazeboRosInterfacePlugin has
+  ///           has loaded and listening to ConnectGazeboToRosTopic and ConnectRosToGazeboTopic messages).
+  void CreatePubsAndSubs();
+
+  ros::NodeHandle *rosnode_;
+
+  sensors::RaySensorPtr parentSensor;
+  transport::NodePtr node_handle_;
+  //transport::PublisherPtr lidar_pub_;
+  ros::Publisher lidar_pub_;
+  std::string namespace_;
+  std::string lidar_topic_;
+
+  /// \brief The connection tied to RayPlugin::OnNewLaserScans()
+private:
+  event::ConnectionPtr newLaserScansConnection;
+  lidar_msgs::msgs::lidar lidar_message;
+};
+} // namespace gazebo
+#endif
---
+++ rotors_gazebo_plugins/src/terabee_manager.cpp
@@ -0,0 +1,53 @@
+#include "ros/ros.h"
+#include "std_msgs/Float32MultiArray.h"
+#include "std_msgs/Float32.h"
+
+#include <sstream>
+
+float terabee_data[8];
+
+void chatterCallback(const std_msgs::Float32MultiArray::ConstPtr& msg)
+{
+  int topic_index = msg->data[0];
+  terabee_data[topic_index] = msg->data[1];
+  // ROS_INFO("I heard: [%f] from [%d]", terabee_data[topic_index], topic_index);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "terabee_manager");
+
+  ros::NodeHandle n;
+  ros::Publisher pub = n.advertise<std_msgs::Float32MultiArray>("/niv1/lidar_terabee", 100);
+
+  ros::Subscriber sub[8];
+  for(int i = 0; i<8; i++){
+    sub[i] = n.subscribe("/niv1/lidar_"+std::to_string(i), 1000, chatterCallback); 
+    ROS_INFO("subscribe /niv1/lidar_%d", i);
+  }
+
+  ros::Rate loop_rate(120);
+
+  while (ros::ok())
+  {
+    std_msgs::Float32MultiArray msg;
+
+    msg.data.clear();
+    std_msgs::MultiArrayDimension dim;
+    dim.label = "ranges";
+    dim.size = 8;
+    dim.stride = 8;
+    msg.layout.dim.push_back(dim);
+
+    for(int i = 0; i<8; i++){
+      msg.data.push_back(terabee_data[i]);
+    }
+
+    pub.publish(msg);
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
