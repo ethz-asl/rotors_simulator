ea887aede2a3ecad088e7c3e4c4a910088838555
michaelpantic <michael.pantic@gmail.com>
2018-12-05 13:45:07 +0100

    Fixes

--- rotors_gazebo_plugins/autobackport/command.sh
+++ rotors_gazebo_plugins/autobackport/command.sh
@@ -3,6 +3,6 @@ find .. -type f -name "*.cpp" | xargs sed -i.bak -r -f seds.txt
 find .. -type f -name "*.h" | xargs sed -i.bak -r -f seds.txt
 find .. -type f -name "*.hpp" | xargs sed -i.bak -r -f seds.txt
 
-sed -i.bak -r -E -f seds_cmake.txt ../CMakeLists.txt
+sed -i.bak -r -f seds_cmake.txt ../CMakeLists.txt
 
 
