7ae8447d0e1e6c874548a98dd09b46af2bfe277c
michaelpantic <michael.pantic@gmail.com>
2018-12-05 13:31:56 +0100

    Sed version incompatibilities

--- rotors_gazebo_plugins/autobackport/command.sh
+++ rotors_gazebo_plugins/autobackport/command.sh
@@ -1,7 +1,7 @@
 
-find .. -type f -name "*.cpp" | xargs sed -i.bak -r -E -f seds.txt
-find .. -type f -name "*.h" | xargs sed -i.bak -r -E -f seds.txt
-find .. -type f -name "*.hpp" | xargs sed -i.bak -r -E -f seds.txt
+find .. -type f -name "*.cpp" | xargs sed -i.bak -r -f seds.txt
+find .. -type f -name "*.h" | xargs sed -i.bak -r -f seds.txt
+find .. -type f -name "*.hpp" | xargs sed -i.bak -r -f seds.txt
 
 sed -i.bak -r -E -f seds_cmake.txt ../CMakeLists.txt
 
