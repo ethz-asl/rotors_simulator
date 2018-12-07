71d01c5f141b864afb2d8a9cc7d0478b62a9136c
michaelpantic <michael.pantic@gmail.com>
2018-12-07 17:48:09 +0100

    test switch for mac

--- rotors_gazebo_plugins/autobackport/commandpre7.sh
+++ rotors_gazebo_plugins/autobackport/commandpre7.sh
@@ -1,7 +1,14 @@
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find . -type f -name "*.cpp" | xargs sed -i.bak -E -f autobackport/seds_pre7.txt
+  find . -type f -name "*.h" | xargs sed -i.bak -E -f autobackport/seds_pre7.txt
+  find . -type f -name "*.hpp" | xargs sed -i.bak -E -f autobackport/seds_pre7.txt
+else
+  find . -type f -name "*.cpp" | xargs sed -i.bak -r -f autobackport/seds_pre7.txt
+  find . -type f -name "*.h" | xargs sed -i.bak -r -f autobackport/seds_pre7.txt
+  find . -type f -name "*.hpp" | xargs sed -i.bak -r -f autobackport/seds_pre7.txt
+fi
 
-find . -type f -name "*.cpp" | xargs sed -i.bak -r -f autobackport/seds_pre7.txt
-find . -type f -name "*.h" | xargs sed -i.bak -r -f autobackport/seds_pre7.txt
-find . -type f -name "*.hpp" | xargs sed -i.bak -r -f autobackport/seds_pre7.txt
+
 
 #sed -i.bak -r -f seds_cmake.txt ../CMakeLists.txt
 
