ecf2412b110ade45e059868b46a6cb6a26af89de
michaelpantic <michael.pantic@gmail.com>
2018-12-10 19:08:21 +0100

    Fix commandpre7.sh

--- rotors_gazebo_plugins/autobackport/commandpre7.sh
+++ rotors_gazebo_plugins/autobackport/commandpre7.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/usr/bin/env bash
 if [[ "$OSTYPE" == "darwin"* ]]; then
   find . -type f -name "*.cpp" | xargs sed -i.bak -E -f autobackport/seds_pre7.txt
   find . -type f -name "*.h" | xargs sed -i.bak -E -f autobackport/seds_pre7.txt
