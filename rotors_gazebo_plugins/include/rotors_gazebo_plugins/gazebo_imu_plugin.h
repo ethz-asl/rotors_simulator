bde59e28a3580ab2885c8f4939f711983789e94f
david-rohr <david.rohr@mavt.ethz.ch>
2019-04-26 12:28:47 +0200

    Modified IMU message (basically now the same as in sitl_gazebo)
    - Added measurement number (seq) to enable discrimination of different measurements

--- rotors_gazebo_plugins/include/rotors_gazebo_plugins/gazebo_imu_plugin.h
+++ rotors_gazebo_plugins/include/rotors_gazebo_plugins/gazebo_imu_plugin.h
@@ -173,6 +173,8 @@ class GazeboImuPlugin : public ModelPlugin {
   Eigen::Vector3d accelerometer_turn_on_bias_;
 
   ImuParameters imu_parameters_;
+
+  uint64_t seq_ = 0;
 };
 
 }  // namespace gazebo
