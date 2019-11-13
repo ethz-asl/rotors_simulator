c29d2bc0e97cc4143d70cf9ac06ac1c2fbba9c8d
kajabo <karen.bodie@gmail.com>
2019-11-13 13:25:13 +0100

    Remove older gazebo version functions.

--- rotors_gazebo_plugins/src/gazebo_mavlink_interface.cpp
+++ rotors_gazebo_plugins/src/gazebo_mavlink_interface.cpp
@@ -119,15 +119,9 @@ void GazeboMavlinkInterface::Load(
             else
               gztopic_[index] =
                   "control_position_gztopic_" + std::to_string(index);
-#if GAZEBO_MAJOR_VERSION >= 7 && GAZEBO_MINOR_VERSION >= 4
-            /// only gazebo 7.4 and above support Any
+                  
             joint_control_pub_[index] =
                 node_handle_->Advertise<gazebo::msgs::Any>(gztopic_[index]);
-#else
-            joint_control_pub_[index] =
-                node_handle_->Advertise<gazebo::msgs::GzString>(
-                    gztopic_[index]);
-#endif
           }
 
           if (channel->HasElement("joint_name")) {
@@ -741,17 +735,9 @@ void GazeboMavlinkInterface::handle_control(double _dt) {
         double force = pids_[i].Update(err, _dt);
         joints_[i]->SetForce(0, force);
       } else if (joint_control_type_[i] == "position_gztopic") {
-#if GAZEBO_MAJOR_VERSION >= 7 && GAZEBO_MINOR_VERSION >= 4
-        /// only gazebo 7.4 and above support Any
         gazebo::msgs::Any m;
         m.set_type(gazebo::msgs::Any_ValueType_DOUBLE);
         m.set_double_value(target);
-#else
-        std::stringstream ss;
-        gazebo::msgs::GzString m;
-        ss << target;
-        m.set_data(ss.str());
-#endif
         joint_control_pub_[i]->Publish(m);
       } else if (joint_control_type_[i] == "position_kinematic") {
         /// really not ideal if your drone is moving at all,
